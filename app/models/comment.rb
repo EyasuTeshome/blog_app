a7300e17015a66f47587c4926652dc62b935ba5e
Josh <joshtesh91@mail.com>
2022-10-27 23:59:08 +0800

    add method for a comment

---
+++ app/models/comment.rb
@@ -0,0 +1,12 @@
+class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_comment_counter
+
+  private
+
+  def update_comment_counter
+    post.increment!(:CommentsCounter)
+  end
+end
