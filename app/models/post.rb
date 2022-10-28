dc2082ba4f1dae8e64f255b39072600c2efc5575
Josh <joshtesh91@mail.com>
2022-10-28 23:35:20 +0800

    initialize test unit by installing rsepc

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,9 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 #validation
-  validates :title, presence: true, length: { maximum: 200 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :Title, presence: true, length: { maximum: 200 }
+  validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def return_recent_comments
     Comment.limit(5).order(created_at: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 #validation
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :Name, presence: true
+  validates :PostCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def return_recent_posts
     Post.limit(3).order(created_at: :desc)
