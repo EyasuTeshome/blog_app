bd019d5d235f7f2924f4932e1c3fdd662b54ff32
Josh <joshtesh91@mail.com>
2022-11-08 18:48:09 +0800

    add test file for post-view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   def current_user
     User.first
+    User.second
   end
 end
---
+++ spec/features/post_show_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Renders the post show page', type: :feature do
+  before :each do
+        @user = User.create(name: 'Bengi', photo: 'https://img.favpng.com/0/6/7/computer-icons-icon-design-user-female-png-favpng-Fsujht93KTGPZWpH8vs1JxSwb.jpg',
+          bio: 'I am Micronaut, studying full stack development', posts_counter: 0)
+    @first_post = Post.create(title: 'Microverse', text: 'Microverse online teaching school for those who want to be developers', comments_counter: 0, likes_counter: 0,
+                author: @user)
+    @second_post = Post.create(title: 'Microverse', text: 'The same post like the first one', comments_counter: 0, likes_counter: 0,
+                author: @user)
+    2.times do |_i|
+    Comment.create(text: 'Nice post!!', author_id: @user.id, post_id: @first_post.id)
+    end
+    visit user_post_path(@first_post.author, @first_post)
+  end
+
+  scenario 'displays the post title' do
+    expect(page).to have_content(@first_post.title)
+  end
+
+  scenario 'shows the person who wrote the post' do
+    expect(page).to have_content('Bengi')
+  end
+
+  it 'can see the username of each commentor.' do
+    expect(page).to have_content('Bengi')
+  end
+
+  scenario 'disolays number of comments' do
+    expect(page).to have_content('Comments: 2')
+  end
+
+  scenario 'displays number of likes' do
+    expect(page).to have_content('Likes: 0')
+  end
+
+  scenario 'displays the content of the post' do
+    expect(page).to have_content(@first_post.text)
+  end
+
+  scenario 'can see the comments of each commentor.' do
+    expect(page).to have_content 'Nice post!!'
+    expect(page).to have_content 'Nice post!!'
+  end
+end
