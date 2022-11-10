eb559951d2ad9dbbb4d903f6ed817fe6dfc0a12e
Josh <joshtesh91@mail.com>
2022-11-11 00:48:24 +0800

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "auth",
+    path_names: {
+      sign_in: "login",
+      sign_out: "logout",
+      password: "secret",
+      confirmation: "verification",
+      unlock: 'unblock',
+      registration: 'register',
+      sign_up: 'cmon_let_me_in'
+  }
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
     resources :posts, only: %i[index show create new] do
--- db/migrate/20221027061802_add_column_to_user.rb
+++ db/migrate/20221027061802_add_column_to_user.rb
@@ -3,6 +3,6 @@ class AddColumnToUser < ActiveRecord::Migration[7.0]
     add_column :users, :name, :string
     add_column :users, :photo, :string
     add_column :users, :bio, :string
-    add_column :users, :posts_counter, :integer
+    add_column :users, :posts_counter, :integer, default: 0
   end
 end
