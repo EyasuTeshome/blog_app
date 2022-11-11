fa96f64440dbb71f122c55cfa355c9c717461e2f
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-11 22:33:57 +0300

    Fix linter issues

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,18 +1,17 @@
 class Api::V1::CommentsController < ApplicationController
   protect_from_forgery with: :null_session
 
-    def index
-      @post = Post.find(params[:post_id])
-      @comments = Comment.where(post_id: params[:post_id])
-      render json: @comments
-
-    end
-
-    def create
-      @post = Post.find(params[:post_id])
-
-      return unless @post.comments.create(text: params[:comment][:text], author: @current_user)
-
-      render json: { success: 'Comment created successfully' }
-    end
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = Comment.where(post_id: params[:post_id])
+    render json: @comments
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+
+    return unless @post.comments.create(text: params[:comment][:text], author: @current_user)
+
+    render json: { success: 'Comment created successfully' }
+  end
+end
