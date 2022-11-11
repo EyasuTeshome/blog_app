7b631c2204019812cdefcd917a814bc32469d693
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-11 14:35:41 +0300

    Add user ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
-
+class Ability
+  include CanCan::Ability
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_094621) do
     t.string "text"
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
-    t.bigint "author_id", null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
